447734a3256302f9e775ec9f3415d6ba9b7e658d
Luciano Livramento (nbk) <livramentoluciano10@gmail.com>
2021-02-22 17:56:39 -0300

    control version: video de fondo en ControlScreen

--- titan_timer_control/lib/view/screens/control_screen.dart
+++ titan_timer_control/lib/view/screens/control_screen.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:titan_timer_control/view/widgets/control/control.dart';
+import 'package:video_player/video_player.dart';
 
-class ControlScreen extends StatelessWidget {
+// Statefull por el video de Background
+class ControlScreen extends StatefulWidget {
+  @override
+  _ControlScreenState createState() => _ControlScreenState();
+}
+
+class _ControlScreenState extends State<ControlScreen> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("assets/videos/gym_video4.mp4");
+
+    _controller.addListener(() => setState(() {}));
+    _controller.setLooping(true);
+    _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
     return Scaffold(
-      appBar: AppBar(title: Text("Control")),
+      appBar: AppBar(
+        title: Text("Control", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
       body: Stack(
         children: [
-          Container(height: size.height, color: Colors.grey[100]),
-          Align(
-            child: ControlCard(),
-            alignment: Alignment.bottomCenter,
+          // Fondo Liso
+          // SizedBox.expand(
+          //   child: Container(
+          //     color: Colors.grey[200],
+          //   ),
+          // ),
+
+          // Video de fondo
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: SizedBox(
+                width: _controller.value.size?.width ?? 0,
+                height: _controller.value.size?.height ?? 0,
+                child: VideoPlayer(_controller),
+              ),
+            ),
           ),
+          Align(child: ControlCard(), alignment: Alignment.bottomCenter),
         ],
       ),
     );
